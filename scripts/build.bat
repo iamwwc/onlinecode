ad7302933068e1547c248bd32be7a0e33c40b62d
iamwwc <qaq1362211689@gmail.com>
2018-10-02 16:08:35 +0800

    fix path

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,6 @@
-cd ../client
+cd ../client-side
 npm install && npm run build
 cd ..
-xcopy client\dist\*.* /YESQ .
+xcopy client-side\dist\*.* /YESQ .
 REM rm client /s
 docker build --tag 0.0.1-alpine3.8 .
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #/usr/bin/bash
 
-cd ../client
+cd ../client-side
 npm install && npm run build
 cd ..
-cp -R client/dist .
+cp -R client-side/dist .
 #rm -rf client
 docker build --tag 0.0.1-alpine .
