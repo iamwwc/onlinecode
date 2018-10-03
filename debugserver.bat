ecfed077e54ab5814abd0ba3b398ff91ce858b65
iamwwc <qaq1362211689@gmail.com>
2018-10-03 22:05:31 +0800

    update docker-compose and remote debug

--- debugserver.bat
+++ debugserver.bat
@@ -3,7 +3,8 @@ docker run -itd --network onlinecode_onlinecode-net ^
     --name debugserver -p 8086:8086 -p 8088:8088 ^
      -e CODE_WORK_DIR="/go/src/chaochaogege.com/onlinecode" ^
     --security-opt=seccomp:unconfined ^
-    ccr.ccs.tencentyun.com/wwc-docker-images/onlinecode:golang-delve
+    ccr.ccs.tencentyun.com/wwc-docker-images/onlinecode:debug-server ^
+     dlv debug --headless --listen=:8088 --api-version=2
 
 
 docker exec -it  debugserver sh
