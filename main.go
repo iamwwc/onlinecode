07bb3033e662f5b54a9694b1a5a11014f7a420cb
iamwwc <qaq1362211689@gmail.com>
2018-10-03 14:26:15 +0800

    fix CI build

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"github.com/onrik/logrus/filename"
 	log "github.com/sirupsen/logrus"
+	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
 	log.SetLevel(log.DebugLevel)
+	file := prepareLogFile("runlog.log")
+	multiWriter := io.MultiWriter(file,os.Stdout)
+	log.SetOutput(multiWriter)
 	hook := filename.NewHook()
 	s, err := newServer(func(s *server) error {
 		//do nothing, for future
@@ -21,6 +26,13 @@ func main() {
 	log.AddHook(hook)
 	log.Infof("Http Server Listen on %v\n", port)
 	log.Errorf("Error when HTTP Listen on %v : %v", port, http.ListenAndServe(":"+port, s))
+}
 
+func prepareLogFile(name string)*os.File{
+	file, err := os.OpenFile(name,os.O_CREATE | os.O_WRONLY , 755)
+	if err != nil{
+		panic(err)
+	}
+	return file
 }
 
--- scripts/mount-container.bat
+++ scripts/mount-container.bat
@@ -3,6 +3,12 @@ REM 启动一个容器，并且将当前文件夹挂载到 golang 镜像下的 /
 REM 在开发环境下，挂在windows temp目录,否则在golang container写入的文件不能经docker daemon 传递到新的container
 REM docker network create -d bridge onlinecode-net
 
-docker run -itd -v %cd%:/go/src/onlinecode -v /var/run/docker.sock:/var/run/docker.sock -v onlinecode_onlinecodetemp:/onlinecodetemp --name test -p 8086:8086 golang tail -f /dev/null
+set GOOS=linux
+set CGO_ENABLED=0
+set GOARCH=amd64
+go build ..
+
+docker run -it -v %cd%:/go/src/onlinecode -v /var/run/docker.sock:/var/run/docker.sock -v onlinecode_onlinecodetemp:/onlinecodetemp ^
+  -p 8086:8086 golang:1.10.4-alpine3.7 sh
 
 REM docker run -itd --name mysqlcontainer -p 3306:3306 --network onlinecode-net -e MYSQL_ROOT_PASSWORD=mysqlpassword mysql:8.0
